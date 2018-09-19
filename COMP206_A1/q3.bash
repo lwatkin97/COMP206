5e24caa2853f136e761dc9aa183a1fd7810e866e
Laura <laura.watkin@mail.mcgill.ca>
2018-09-19 15:30:26 -0400

    Changed q3.bash

---
+++ COMP206_A1/q3.bash
@@ -0,0 +1,27 @@
+
+parent=$1
+
+files=`ls -lR $parent| sort -k8| grep .jpg| cut -f11 -d " "`
+
+changeName=`echo "$parent"|tr '/' '_'`
+
+outputFile="$changeName.jpg"
+
+convert_append=$files $outputFile
+
+
+
+
+
+
+###directories=`ls $parent`
+#echo $directories
+#mkdir newFiles
+#list=
+#for file in $directories
+#do
+    #findFile=`ls $parent/$file`
+    #list="${list} ${findFile}"
+#done
+
+#ls -t {$list}
